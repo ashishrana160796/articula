02f4420d681cbc350126f6654a1b945ca4321837
Ashish Rana <ashishrana160796@gmail.com>
2023-12-30 14:06:56 +0100

    added ghostbusters data processing util

--- common/data_utils.py
+++ common/data_utils.py
@@ -4,13 +4,14 @@ import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
 from typing import Optional
+from collections import OrderedDict
 from common import constants as const
 
 random.seed(16) # fixing the random seed for retrieving fixed data subset
 
-def load_intrinsic_dim_dataset(data_path: str | Path, subset_fraction: Optional[float]=1.):
+def intrinsic_dim_dataset_to_csv(data_path: str | Path, subset_fraction: Optional[float]=1.):
     """
-    It loads data subset for the intrinsic dimension dataset,
+    It loads data subset for the intrinsic dimension dataset into a single csv file,
     with additional feature to load fraction of data subsets.
     """
     if not isinstance(data_path, Path):
@@ -50,11 +51,39 @@ def load_intrinsic_dim_dataset(data_path: str | Path, subset_fraction: Optional[
     trasformed_data_df = pd.concat([human_text_data_df, ai_gen_text_data_df], axis=0)
     trasformed_data_df.to_csv(Path(const.TRANSFORMED_DATA_SAVE_PATH) / "transformed_intrinsic_dim_data.csv", index=False)
 
+
+def intrinsic_dim_dataset_to_files(data_path: str | Path, subset_fraction: Optional[float]=1.):
+    """
+    It loads data subset for the intrinsic dimension dataset into text files with data subset folders,
+    with additional feature to load fraction of data subsets.
+    """
+    if not isinstance(data_path, Path):
+        data_path = Path(data_path)
+    dataset_file_paths = [entity for entity in data_path.glob('**/*') if entity.is_file()]
+
+    dataset_file_paths_dict = OrderedDict()
+    for file in dataset_file_paths:
+        dataset_file_paths_dict[file] = [Path(file_ext) for file_ext in file.name.split(".")[0].split("_")]
+    
+    for (dataset_file_path, dataset_ext_list) in tqdm(dataset_file_paths_dict.items()):
+
+        # intrinsic dimension dataset creation in GHOSTBUSTERS format
+        intrinsic_dim_data = Path(const.INTRINSIC_DIM_DATA_STR)
+        human_data_path = const.TRANSFORMED_DATA_SAVE_PATH / intrinsic_dim_data / dataset_ext_list[-1] / Path("human")
+        ai_data_path = const.TRANSFORMED_DATA_SAVE_PATH / intrinsic_dim_data / dataset_ext_list[-1] / Path("ai")
+        human_data_path.mkdir(parents=True, exist_ok=True)
+        ai_data_path.mkdir(parents=True, exist_ok=True)
+
+        with open(dataset_file_path) as json_data_file:
+            json_list = json.load(json_data_file)
+            IDX_VAL = 1
+            for json_value in json_list:
+                with open(human_data_path / f"{dataset_ext_list[0]}_{IDX_VAL}_", "w") as f:
+                    f.write(json_value['prefix']+" "+json_value['gold_completion'])
+                with open(ai_data_path / f"{dataset_ext_list[1]}_{IDX_VAL}", "w") as f:
+                    f.write(json_value['prefix']+" "+ ''.join(json_value['gen_completion']))
+                IDX_VAL += 1
+
 if __name__ == '__main__':
-    sample_text = "Speaking of festivities, there is one day in China that stands unrivaled - \
-                   the first day of the Lunar New Year, commonly referred to as the Spring Festival. \
-                   Even if you're generally uninterested in celebratory events, it's hard to resist the \
-                   allure of the family reunion dinner, a quintessential aspect of the Spring Festival. \
-                   Throughout the meal, family members raise their glasses to toast one another, expressing wishes \
-                   for happiness, peace, health, and prosperity in the upcoming year."
-    load_intrinsic_dim_dataset(const.INTRINSIC_DIM_DATASET, subset_fraction=.025)
+    # intrinsic_dim_dataset_to_csv(const.INTRINSIC_DIM_DATASET, subset_fraction=1.)
+    intrinsic_dim_dataset_to_files(const.INTRINSIC_DIM_DATASET)
