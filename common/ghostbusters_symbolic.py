310532d338b324244c31283d515c57680a3adf1b
Sharan <sharanshyamsundar@gmail.com>
2024-01-01 22:21:59 +0100

    ghostbusters common stuff update

--- common/ghostbusters_symbolic.py
+++ common/ghostbusters_symbolic.py
@@ -7,8 +7,8 @@ import numpy as np
 import tiktoken
 import dill as pickle
 
-from utils.featurize import *
-from utils.n_gram import *
+from common.ghostbusters_featurize import *
+from common.ngram import *
 
 from collections import Counter, defaultdict
 from sklearn.linear_model import LogisticRegression
