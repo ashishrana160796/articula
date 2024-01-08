5b9624408e4644ed17b3fbe4a038f34889d6f688
Ashish Rana <ashishrana160796@gmail.com>
2024-01-08 03:28:34 +0100

    german dataset added for small and large texts

--- data_generator/generate_dataset_en.py
+++ data_generator/generate_dataset_en.py
@@ -12,7 +12,7 @@ random.seed(16)
 nltk.download('brown')
 
 
-def create_base_corpus(savepath: str | Path, file_name: str):
+def create_base_en_corpus(save_path: str | Path, file_name: str):
     text_categories = brown.categories()
     category_list, context_text_list, completion_text_list = [], [], []
     for category in text_categories:
@@ -47,16 +47,16 @@ def create_base_corpus(savepath: str | Path, file_name: str):
             'human_text': completion_text_list,
         })
 
-    return brown_data_df.to_csv(Path(savepath) / file_name, index=False)
+    brown_data_df.to_csv(Path(save_path) / file_name, index=False)
 
 
-def generate_gpt_text(data_path: str | Path, file_name: str):
+def generate_gpt_en_text(data_path: str | Path, file_name: str):
     data_df = pd.read_csv(Path(data_path) / file_name)
     data_df["ai_text_dtr"] = gen_model.GPT2Generator(list(data_df["prompt_text"]), min_length=8, max_length=140).generate_text()
     data_df["ai_text_stc"] = gen_model.GPT2Generator(list(data_df["prompt_text"]), min_length=8, max_length=140, gen_method='stc').generate_text()
     data_df["ai_text_con"] = gen_model.GPT2Generator(list(data_df["prompt_text"]), min_length=8, max_length=140, gen_method='con').generate_text()
-    return data_df.to_csv(Path(data_path) / file_name, index=False)
+    data_df.to_csv(Path(data_path) / file_name, index=False)
 
 if __name__ == '__main__':
-    create_base_corpus(const.TRANSFORMED_DATA_SAVE_PATH, const.GEN_EN_DATA_CSV_NAME)
-    generate_gpt_text(const.TRANSFORMED_DATA_SAVE_PATH, const.GEN_EN_DATA_CSV_NAME)
+    create_base_en_corpus(const.TRANSFORMED_DATA_SAVE_PATH, const.GEN_EN_DATA_CSV_NAME)
+    generate_gpt_en_text(const.TRANSFORMED_DATA_SAVE_PATH, const.GEN_EN_DATA_CSV_NAME)
